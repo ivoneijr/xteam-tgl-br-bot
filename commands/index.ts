d741ea35f69e4264c361b5e54a7255a082471adc
ivonei junior <ivoneijr@gmail.com>
2021-12-07 12:49:17 -0300

    add invoice infos

--- commands/index.ts
+++ commands/index.ts
@@ -1,5 +1,34 @@
-export const whoAmI = ctx => {
+import { Context } from 'telegraf';
+
+export const help = (ctx: Context) => {
+  const message = `The bot just repeats anything you say in the chat.
+    \n*Command reference:*
+    /ping - *Pong!*
+    /whoami - Informações sobre você.
+    /help - Mostra essa mensagem.`;
+
+  return ctx.replyWithMarkdown(message);
+};
+
+export const whoAmI = (ctx: Context) => {
   const userInfo = JSON.stringify(ctx.from);
 
   return ctx.reply(`User info: ${userInfo}`);
+};
+
+export const ping = (ctx: Context) => {
+  return ctx.replyWithMarkdown('*Pong!*');
+};
+
+export const invoiceInfos = (ctx: Context) => {
+  return ctx.reply(`
+    X-Team International Pty Ltd.
+    PO Box 537
+    Chelsea, VIC 3196
+    Australia
+    Tax ID: 95 160 801 301
+    Australia
+    +1 863-225-4802
+    contact@x-team.com
+  `);
 };
--- initializers/bot.ts
+++ initializers/bot.ts
@@ -1,5 +1,6 @@
 import { Telegraf } from 'telegraf';
-import { whoAmI } from '../commands';
+import { help, whoAmI, ping, invoiceInfos } from '../commands';
+
 export const bot = new Telegraf(process.env.BOT_TOKEN!, {
   telegram: { webhookReply: true },
 });
@@ -12,24 +13,8 @@ bot.start(ctx => {
   );
 });
 
-bot.help(ctx => {
-  return ctx.replyWithMarkdown(`The bot just repeats anything you say in the chat.
-  \n*Command reference:*
-      /ping - *Pong!*
-      /whoami - Informações sobre você.
-      /help - Mostra essa mensagem.`);
-});
+bot.help(help);
 
 bot.command('whoami', whoAmI);
-
-bot.command('ping', ctx => {
-  return ctx.replyWithMarkdown('*Pong!*');
-});
-
-bot.command('hello', ctx => {
-  return ctx.replyWithMarkdown('*worldsssss!!!!*');
-});
-
-// bot.on('text', ctx => {
-//   return ctx.reply(ctx.message.text);
-// });
+bot.command('ping', ping);
+bot.command('invoice_infos', invoiceInfos);
